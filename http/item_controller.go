91d99ea5358c7e26650f9f59d42d370077826245
Juan Jose Barrera <barrerajuanjose@gmail.com>
2020-12-20 10:13:35 -0300

    added buyer nickname

--- http/item_controller.go
+++ http/item_controller.go
@@ -5,6 +5,7 @@ import (
 	"github.com/barrerajuanjose/item_deco/marshaller"
 	"github.com/barrerajuanjose/item_deco/service"
 	"github.com/gin-gonic/gin"
+	"strconv"
 	"sync"
 )
 
@@ -28,30 +29,43 @@ func NewItemController(itemMarshaller marshaller.ItemMarshaller, itemService ser
 
 func (c itemController) Get(ctx *gin.Context) {
 	itemId := ctx.Param("item_id")
-	var itemDomain *domain.Item
+	buyerId := ctx.Query("buyer_id")
 
 	itemChan := make(chan *domain.Item, 1)
 	sellerChan := make(chan *domain.User, 1)
+	buyerChan := make(chan *domain.User, 1)
 
 	var wg sync.WaitGroup
-	wg.Add(2)
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		defer close(buyerChan)
+		if buyerInt, err := strconv.ParseInt(buyerId, 10, 32); err == nil {
+			buyerChan <- c.userService.GetUserById(int32(buyerInt))
+		} else {
+			buyerChan <- nil
+		}
+	}()
 
 	go func() {
 		defer wg.Done()
 		defer close(itemChan)
-		itemChan <- c.itemService.GetItemById(itemId)
-	}()
+		item := c.itemService.GetItemById(itemId)
+		itemChan <- item
 
-	go func() {
-		defer wg.Done()
-		defer close(sellerChan)
-		itemDomain = <- itemChan
-		sellerChan <- c.userService.GetUserById(itemDomain.SellerId)
+		go func() {
+			defer wg.Done()
+			defer close(sellerChan)
+			sellerChan <- c.userService.GetUserById(item.SellerId)
+		}()
 	}()
 
 	wg.Wait()
 
+	itemDomain := <- itemChan
 	sellerDomain := <- sellerChan
+	buyerDomain := <- buyerChan
 
-	ctx.JSON(200, c.itemMarshaller.GetView(itemDomain, sellerDomain))
+	ctx.JSON(200, c.itemMarshaller.GetView(itemDomain, sellerDomain, buyerDomain))
 }
--- marshaller/item_marshaller.go
+++ marshaller/item_marshaller.go
@@ -6,13 +6,14 @@ type ItemDto struct {
 	Title              string `json:"title,omitempty"`
 	Permalink          string `json:"permalink,omitempty"`
 	SellerNickname     string `json:"seller_nickname,omitempty"`
+	BuyerNickname     string `json:"buyer_nickname,omitempty"`
 	ShippingMode       string `json:"shipping_mode,omitempty"`
 	BestPaymentMethod  string `json:"best_payment_method,omitempty"`
 	BestShippingMethod string `json:"best_shipping_method,omitempty"`
 }
 
 type ItemMarshaller interface {
-	GetView(item *domain.Item, seller *domain.User) *ItemDto
+	GetView(item *domain.Item, seller *domain.User, buyer *domain.User) *ItemDto
 }
 
 type itemMarshaller struct {
@@ -23,10 +24,16 @@ func NewItemMarshaller() ItemMarshaller {
 	return &itemMarshaller{}
 }
 
-func (m itemMarshaller) GetView(item *domain.Item, seller *domain.User) *ItemDto {
+func (m itemMarshaller) GetView(item *domain.Item, seller *domain.User, buyer *domain.User) *ItemDto {
+	var buyerNickname string
+	if buyer != nil {
+		buyerNickname = buyer.Nickname
+	}
+
 	return &ItemDto {
 		Title:          item.Title,
 		Permalink:      item.Permalink,
 		SellerNickname: seller.Nickname,
+		BuyerNickname: buyerNickname,
 	}
 }
